1518453f1cdae43887a98f7b5d70fa8a8b5d58cc
herizon1054 <spivbaby@gmail.com>
2026-04-09 17:28:23 +0800

    Update acm.py

--- dragonpilot/selfdrive/controls/lib/acm.py
+++ dragonpilot/selfdrive/controls/lib/acm.py
@@ -42,8 +42,8 @@ SOFT_HOLD_RANGE_MAX = 0.99
 SOFT_HOLD_TTC_THRESHOLD = 3.0          # [新增] TTC 大於此數值時，暫停 Soft Hold 運作
 
 # 車速 (km/h) 對應 最高加速度限制 (m/s²) 的插值陣列
-SOFT_HOLD_SPEED_BP = [10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0]
-SOFT_HOLD_ACCEL_V  = [ 0.7,  0.6,  0.5,  0.4,  0.3,  0.2,  0.1]
+SOFT_HOLD_SPEED_BP = [10.0, 20.0, 30.0, 40.0, 50.0]
+SOFT_HOLD_ACCEL_V  = [ 0.48,  0.32,  0.24,  0.16,  0.08]
 
 
 class ACM:
