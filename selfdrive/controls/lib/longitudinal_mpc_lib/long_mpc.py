b4c903bffc51eaf1adcdb95e69127a7fdc7a3962
herizon1054 <spivbaby@gmail.com>
2026-04-29 14:48:36 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -74,9 +74,9 @@ def get_T_FOLLOW(personality=log.LongitudinalPersonality.standard):
   if personality==log.LongitudinalPersonality.relaxed:
     return 1.75
   elif personality==log.LongitudinalPersonality.standard:
-    return 1.35
+    return 1.45
   elif personality==log.LongitudinalPersonality.aggressive:
-    return 1.00
+    return 1.25
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
