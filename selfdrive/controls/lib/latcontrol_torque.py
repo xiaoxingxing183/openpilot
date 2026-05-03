e1fb537f40e39151c9dd6c1e4720bcd26c7d0c77
herizon1054 <spivbaby@gmail.com>
2026-05-03 10:33:13 +0800

    Update latcontrol_torque.py

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -21,7 +21,7 @@ from openpilot.common.pid import PIDController
 # to be overcome to move it at all, this is compensated for too.
 
 KP = 1.0
-KI = 0.25
+KI = 0.3
 KD = 0.0
 INTERP_SPEEDS = [1, 1.5, 2.0, 3.0, 5, 7.5, 10, 15, 20, 30]
 KP_INTERP = [200, 120, 65, 30, 11.5, 8.5, 5.5, 3.0, 2.0, KP]
